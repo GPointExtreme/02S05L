49aa26ea66fbac11ca75d0afcbe5a1ec5815d9e3
GPointExtreme <dominik.geiger@hotmail.com>
2016-05-20 19:52:15 +0200

    Time Server neu gemacht.

---
+++ src/networking/lv/time/TimeClient.java
@@ -0,0 +1,28 @@
+package networking.lv.time;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TimeClient {
+
+	public static void main(String[] args) {
+		try (
+			Socket client = new Socket("127.0.0.1", 1111);
+			InputStreamReader isr = new InputStreamReader(client.getInputStream());
+			BufferedReader br = new BufferedReader(isr);
+			) {
+			
+			System.out.println(br.readLine());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
---
+++ src/networking/lv/time/TimeServer.java
@@ -0,0 +1,34 @@
+package networking.lv.time;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+
+public class TimeServer {
+
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(1111);
+			
+			for (int i = 1; i < 3; i++) {
+			Socket client = server.accept();
+
+				try (
+					OutputStreamWriter osw = new OutputStreamWriter(client.getOutputStream());
+					BufferedWriter bw = new BufferedWriter(osw);
+					) {
+						bw.write(LocalDateTime.now().toString()+" "+i+"\n");
+						bw.flush();
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
